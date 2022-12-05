4886f4910f339dd31da23b0cb65e9b839680bdb9
BlackDemonFire <lucy@blackdemon.codes>
2022-12-05 22:56:47 +0100

    Day 5

---
+++ d05t1/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    let (base, actions) = input.split_once("\n\n").unwrap();
+    let cols: [&mut Vec<char>; 9] = [
+        &mut Vec::new(),
+        &mut Vec::new(),
+        &mut Vec::new(),
+        &mut Vec::new(),
+        &mut Vec::new(),
+        &mut Vec::new(),
+        &mut Vec::new(),
+        &mut Vec::new(),
+        &mut Vec::new(),
+    ];
+    for line in base.lines().rev().skip(1) {
+        for col in 0..9 {
+            let char = line.chars().nth((col * 4) + 1).unwrap();
+            if !char.is_whitespace() {
+                cols[col].push(char);
+            }
+        }
+    }
+    for line in actions.lines().map(extract_action) {
+        for _ in 0..line.0 {
+            let moving_value = cols[line.1 as usize - 1].pop().unwrap();
+            cols[line.2 as usize - 1].push(moving_value);
+        }
+    }
+    let result = cols
+        .map(|col| col.pop().unwrap())
+        .iter()
+        .collect::<String>();
+    println!("{}", result)
+}
+
+fn extract_action(line: &str) -> (u8, u8, u8) {
+    let mut args = line.split_whitespace();
+    let amount: u8 = args.nth(1).unwrap().parse().unwrap();
+    let from: u8 = args.nth(1).unwrap().parse().unwrap();
+    let to: u8 = args.nth(1).unwrap().parse().unwrap();
+    (amount, from, to)
+}
