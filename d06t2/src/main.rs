9d5d483ad572d22855d6b5e2a8c4e1d96fb584b7
BlackDemonFire <lucy@blackdemon.codes>
2022-12-06 10:33:19 +0100

    Day 6

---
+++ d06t2/src/main.rs
@@ -0,0 +1,19 @@
+use std::{collections::VecDeque, fs::read_to_string};
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    let mut storage = VecDeque::<char>::new();
+    for (index, char) in input.chars().enumerate() {
+        storage.push_back(char);
+        if storage.len() > 14 {
+            storage.pop_front();
+            if !storage
+                .iter()
+                .any(|c| storage.iter().filter(|d| &c == d).collect::<Vec<_>>().len() > 1)
+            {
+                println!("{}", index + 1);
+                break;
+            }
+        }
+    }
+}
